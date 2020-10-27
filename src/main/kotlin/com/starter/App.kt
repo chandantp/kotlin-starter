0bdc1cb85762891a153986c9aabec037194a959d
Chandan Tumkur Prakash (p763833) <Chandan.Tumkur.Prakash@nab.com.au>
2020-10-28 08:06:08 +1100

    Added mockk dependencies & sample test

--- src/main/kotlin/com/starter/App.kt
+++ src/main/kotlin/com/starter/App.kt
@@ -3,3 +3,7 @@ package com.starter
 fun main() {
     println("Hello!")
 }
+
+fun foo(): Long = System.currentTimeMillis()
+
+fun bar(): Long = foo() * 2
--- src/test/kotlin/com/starter/AppTest.kt
+++ src/test/kotlin/com/starter/AppTest.kt
@@ -2,16 +2,28 @@ package com.starter
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import io.mockk.every
+import io.mockk.mockkStatic
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object AppTest: Spek({
 
-    describe("a calculator") {
+    describe("sample tests") {
 
         context("adding") {
             it("2 + 3 equals 5") {
                 assertThat(2 + 3, equalTo(5))
+            }
+        }
+
+        context("mocking foo") {
+
+            mockkStatic("com.starter.AppKt")
+            every { foo() } returns 123L
+
+            it("bar returns expected value") {
+                assertThat(bar(), equalTo(246L))
             }
         }
     }
